c822e9b84f01c95f61fa831634e799e4095fd0b7
LeacHar <2964556627@qq.com>
2021-07-23 18:14:26 +0800

    调整搜索框出现位置

--- nav.php
+++ nav.php
@@ -34,24 +34,17 @@
 
                 <div class="navbar-end is-hidden-touch">
                     <div class="navbar-item">
-                        <form id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
-                            <div class="search-wrap" style="position: relative;"
-                                 onmouseover="$('.search-input').focus()">
-                                <span>
-                                    <input type="text" id="s" style="opacity: 0;" name="s" class="search-input"
-                                           placeholder="<?php _e('输入关键字搜索'); ?>" required/>
-                                </span>
-                                <span class="buttons">
-                                    <span class="icon">
-                                        <a onclick="$('#search').submit()" class="action">
-                                            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                                                <path fill="currentColor"
-                                                      d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/>
-                                            </svg>
-                                        </a>
-                                    </span>
-                                </span>
-                            </div>
+                        <form id="search" method="post" action="<?php /*$this->options->siteUrl(); */?>" role="search">
+                            <label for="searchInput" class="search-wrap">
+                            <a class="icon" style="color: #000;">
+                                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                    <path fill="currentColor"
+                                          d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"/>
+                                </svg>
+                            </a>
+                            <input type="text" id="searchInput" name="s" class="search-input"
+                                   placeholder="<?php _e('输入关键字搜索'); ?>" required/>
+                            </label>
                         </form>
                     </div>
                 </div>
