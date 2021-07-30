4b0fcc92a6fdd6149e433f3fa0162dcebd89f37c
Chunping.Liao <2964556627@qq.com>
2021-07-30 14:52:53 +0800

    去除谷歌字体

--- header.php
+++ header.php
@@ -23,9 +23,6 @@
         ), '', ' - '); ?><?php $this->options->title(); ?></title>
     <link rel="shortcut icon" href="/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simplelightbox@2.8.0/dist/simple-lightbox.min.css">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('static/css/style.css'); ?>">
     <?php if ($this->fields->keywords || $this->fields->desc) : ?>
